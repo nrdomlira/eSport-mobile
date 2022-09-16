228fd6ed786e95394201d56e3ab0818c1c6d80c3
José Domingos <domliraa@gmail.com>
2022-09-16 14:43:48 -0300

    Add modal user discord

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -12,17 +12,26 @@ import { styles } from './styles';
 import { THEME } from '../../theme';
 import { Heading } from '../../components/Heading';
 import { DuoCard, DouCardProps } from '../../components/DuoCard';
+import { DuoMatch } from '../../components/DuoMatch';
 
 
 
 export function Game() {
     const [duos, setDuos] = useState<DouCardProps[]>([])
+    const [discordDuoSelected, setDiscordDuoSelected] = useState('');
+
     const navigation = useNavigation();
     const route = useRoute();
     const game = route.params as GameParams;
 
     function handleGoBack() {
         navigation.goBack();
+    }
+
+    async function getDiscordUser(adsId: string) {
+        fetch(`http://192.168.1.5:3333/ads/${adsId}/discord`)
+            .then(response => response.json())
+            .then(data => setDiscordDuoSelected(data.discord))
     }
 
     useEffect(() => {
@@ -65,7 +74,7 @@ export function Game() {
                         data={duos}
                         keyExtractor={item => item.id}
                         renderItem={({ item }) => (
-                            <DuoCard data={item} onConnect={() => { }} />
+                            <DuoCard data={item} onConnect={() => setDiscordDuoSelected(item.id)} />
                         )}
                         horizontal
                         style={styles.containerList}
@@ -76,6 +85,12 @@ export function Game() {
                                 Não há anúncios publicados ainda.
                             </Text>
                         )}
+                    />
+
+                    <DuoMatch
+                        visible={discordDuoSelected.length > 0}
+                        discord={discordDuoSelected}
+                        onClose={() => setDiscordDuoSelected('')}
                     />
                 </SafeAreaView>
             </Background>
